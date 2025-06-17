bf9b51669ec656f4d2406d9160379fcef7cc1877
17Shashidhar <shashidhar17@gmail.com>
2025-06-17 22:45:49 +0530

    this is the clone of the Discord

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+  fontfamily:{
+    Whiteny:["Whiteny"],
+    Ginto:["Ginto"],
+    ggSans:["ggSans"],
+  },
+}
+
